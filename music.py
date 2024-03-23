7254b12880127e4687b15d305fbca941f328ea27
Amrit <100637966+Chand7577@users.noreply.github.com>
2024-03-23 08:48:41 +0530

    Update music.py

--- music.py
+++ music.py
@@ -5,7 +5,8 @@ mixer.music.load('dark.mp3')
 mixer.music.play()
 
 while True:
-    inp=input("Enter your choice..l...")
+    print('Enter p to pause or u to unpause music')
+    inp=input("Enter your choice.....")
     if inp=='p':
         mixer.music.pause()
 
